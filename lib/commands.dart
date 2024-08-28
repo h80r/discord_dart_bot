b3e0653e2a39a3c426b3adab5654da882ebbcf25
h80r <h80rgbc@gmail.com>
2024-08-28 09:57:36 -0300

    💬 Reduce command description text

--- lib/commands.dart
+++ lib/commands.dart
@@ -1,11 +1,10 @@
 import 'package:nyxx/nyxx.dart';
 import 'package:nyxx_commands/nyxx_commands.dart';
 
+// Description field must be 100 characters or less
 final bfCommand = ChatCommand(
   'bf',
-  'Quando o mano tá perdidinho na porradaria,'
-      ' a mente não consegue formular uma frase direito,'
-      ' esquecendo de tudo, é o _brain fog_',
+  'Quando a mente do mano não consegue formular uma frase direito, esquecendo de tudo, é o _brain fog_',
   (ChatContext ctx) async => await ctx.respond(
     MessageBuilder(content: '🧠🌫️'),
   ),
