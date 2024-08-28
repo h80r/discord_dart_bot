8e91b49e64a029a8307841b1aa746a16a6afc51a
h80r <h80rgbc@gmail.com>
2024-08-28 09:54:40 -0300

    🐛 Fix command not available bug

--- lib/commands.dart
+++ lib/commands.dart
@@ -1,6 +1,12 @@
 import 'package:nyxx/nyxx.dart';
 import 'package:nyxx_commands/nyxx_commands.dart';
 
-final bfCommand = ChatCommand('bf', 'Brainfog command', (context) async {
-  await context.respond(MessageBuilder(content: '🧠 🌫️'));
-});
+final bfCommand = ChatCommand(
+  'bf',
+  'Quando o mano tá perdidinho na porradaria,'
+      ' a mente não consegue formular uma frase direito,'
+      ' esquecendo de tudo, é o _brain fog_',
+  (ChatContext ctx) async => await ctx.respond(
+    MessageBuilder(content: '🧠🌫️'),
+  ),
+);
